08fce888c7b227f3bf5a2e7273ffc5fb7ab0ff59
PanPeryskop <stanislaw.gadek@o365.us.edu.pl>
2026-01-08 16:44:29 +0100

    first commit

---
+++ src/main/java/org/quality5/Quality5Application.java
@@ -0,0 +1,13 @@
+package org.quality5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quality5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quality5Application.class, args);
+    }
+
+}
